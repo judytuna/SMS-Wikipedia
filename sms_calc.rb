3478aa4644df6d9c45525750503f94af7f035845
Judy Tuan <judytuna@gmail.com>
2011-11-07 00:40:47 -0800

    testing some more

--- sms_calc.rb
+++ sms_calc.rb
@@ -23,7 +23,7 @@ post '/calc' do
   mycalc = RPNCalculator.new
   myanswer = mycalc.evaluate(params[:Body])
 
-  "<Response><Sms> Hello.</Sms></Response>"
+  "<Response><Sms> Hello." + "a" + "</Sms></Response>"
   # " + params[:Body] + " evaluates to " + myanswer + "
 end
 
