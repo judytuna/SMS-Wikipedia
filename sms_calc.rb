dbbec8c5851b61e522a1876cc2ddcf143a361c81
Judy Tuan <judytuna@gmail.com>
2012-01-21 15:09:35 -0800

    lol

--- sms_calc.rb
+++ sms_calc.rb
@@ -9,7 +9,14 @@ account_sid = 'AC60c309b5c40342009c38e91c468ff41a'
 auth_token = '45c473926f66f85d5ec3bf82f318c305'
 
 get '/' do
-  "Hi! Call or text (415) 599-2671 with Judy's PIN. Text app not done yet =)"
+  <<END
+  Hi! Call or text (415) 599-2671 with Judy's PIN. Text app not done yet =)
+  <form>
+   <label for="page">Page:</label><input type="text" name="page" size="20">
+   <br/><input type="submit" value="submit"/>
+  </form>
+END
+
   # set up a client to talk to the Twilio REST API
 #  @client = Twilio::REST::Client.new account_sid, auth_token
 
@@ -24,7 +31,7 @@ get '/' do
 #  )
 end
 
-post '/calc' do
+get '/calc' do
   proxy = ENV['HTTP_PROXY']
   clnt = HTTPClient.new(proxy)
   clnt.set_cookie_store("cookie.dat")
@@ -43,7 +50,7 @@ post '/calc' do
   )
 end
 
-post '/hi' do
+get '/hi' do
   file = open('http://en.wikipedia.org/wiki/Hedy_Lamarr')
   contents = file.read
   "<Response><Say>" + contents + "</Say></Response>"
